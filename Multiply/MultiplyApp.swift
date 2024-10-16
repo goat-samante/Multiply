3c2425100fb1fbe6f6a53cb1cbbe353290c4ea38
Samuel Amante <amantsa76@gmail.com>
2024-10-16 09:53:51 -0500

    Initial Commit

---
+++ Multiply/MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyApp.swift
+//  Multiply
+//
+//  Created by Samuel Amante on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
